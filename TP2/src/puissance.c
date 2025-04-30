d9ee89dcabfa64fb385a6a8225c66759b064a791
Leventrak <cfastlambertkm@gmail.com>
2025-04-30 15:39:38 +0200

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -4,13 +4,8 @@ int main() {
     int a = 2;     
     int b = 3;     
     int resultat = 1;
-
-      if (b < 0) {
-        printf("\n");
-        return 1;
-    }
-
-    for (int i = 0; i < b; i++) {
+    
+    for (int i = 0; i != b; i++) {
         resultat *= a;
     }
 
